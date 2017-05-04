1566269eb39e828621758b303625e2d68b970ac1
Austin Hutchinson <Ahutch95@gmail.com>
2017-05-04 14:47:38 -0700

    Exercise 1

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,25 +2,27 @@
 
 # Install and load `ggplot2`
 # You will also want to load `dplyr`
-
+install.packages("ggplot2")
+library(ggplot2)
+library(dplyr)
 
 # For this exercise you'll be working with the `diamonds` data set included in the ggplot2 library
 # Use `?diamonds` to get more information about this data set (including the column descriptions
 # Also check the _column names_ and the _number of rows_ in the data set
-
+?diamonds
 
 # This data set has a lot of rows. To make things a bit more readable,
 # use dplyr's `sample_n()` function to get a random 1000 rows from the data set
 # Store this sample in a variable `diamonds.sample`
 
-
+sample.data <- sample_n(tbl = diamonds,size = 1000)
 # Start by making a new `ggplot` with the `diamonds.sample` as the data (no geometry yet)
 # What do you see?
-
-
+ggplot(data = sample.data)
+?ggplot
 # Draw a scatter plot (with point geometry) with for the `diamonds.sample` set,
 # with the `carat` mapped to the x-position and `price` mapped to the y-position.
-
+ggplot(data = sample.data) + geom_point(mapping = aes(x = carat, y = price, color = clarity))
 
 # Draw the same plot as above, but color each of the points based on their clarity.
 
